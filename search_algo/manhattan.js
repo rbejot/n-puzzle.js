0363733f15baf3a1d09e42e0a7d003cf6844edab
Rodolphe <rbejot@student.42.fr>
2018-09-15 17:20:45 +0200

    [feature]heuristic calculation ok for manhattan

--- search_algo/manhattan.js
+++ search_algo/manhattan.js
@@ -9,6 +9,18 @@ Log = (size) => {
 }
 
 heuristic = (current, goal, n_size) => {
+  let h = 0
+  for (let i = 0; i < n_size; i++) {
+    for (let j = 0; j < n_size; j++) {
+      if (current[i][j] != goal[i][j] && current[i][j] != 0) {
+        let good_column = Math.ceil(current[i][j] / n_size - 1)
+        let good_index = current[i][j] % n_size - 1
+        if (good_index == -1)
+          good_index = n_size - 1
+        h += Math.abs(good_column - i) + Math.abs(good_index - j)
+      }
+    }
+  }
   return h
 }
 
@@ -16,7 +28,7 @@ manhattan = (n_size, start, goal) => {
   let h = heuristic(start, goal, n_size)
   let g = 0
   let f = g + h
-  Log({n_size, start, goal})
+  Log({n_size, start, goal, h})
   return start
 }
 
